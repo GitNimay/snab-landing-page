db19d8a9997822adb5d3d561453e4df9af7e778b
Nimesh Kulkarni <167794819+GitNimay@users.noreply.github.com>
2026-07-17 18:50:23 +0530

    feat: update next.config.mjs with image formats and experimental settings for optimization

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,16 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 2678400,
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
 };
 
 const withMDX = createMDX({});
